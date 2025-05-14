45a5a788e10151d69438cacec3a1b90607d30eb3
buddypall1 <buddypall321@gmail.com>
2025-05-14 19:18:37 +0200

    Work on enemy param settings
    + removed some unneeded comments, added message boxes for errors and successes. Added plans for AI RNG

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import tkinter
 from tkinter import ttk
+import tkinter.messagebox
+
+
+# ENEMY RNG TABLE DEFAULTS: 50
+# For attack: This will be default when nothing is needed
+# When below half health: it will heal if RNG rolls below selected RNG value, will attack if above, which means higher RNG = harder fight. (100 would mean 100% chance, 0 would mean 0%)
+
 
 #### window creation start ####
 window = tkinter.Tk()
@@ -26,8 +33,10 @@ class Enemparams():
         self.rng = rng
         self.weapon = weapon
 
-player = Playerparams(100, 5, 'None')
 
+#### default values, should be written differently using saved settings once that's implemented ####
+player = Playerparams(100, 5, 'None')
+enemy = Enemparams(100, 5, 5, 'None')
 #### UI buttons handling ####
 def exitpressed():
     window.destroy()
@@ -57,36 +66,40 @@ def settingspressed():
     health_entry.place(x=160, y=150)
     playerlabel = tkinter.Label(settingwindow, text="Player", font=('Arial', 25))
     playerlabel.place(x=160, y= 100)
+    
     def update_health():
         try:
             new_health = int(health_entry.get())
-            player.health = new_health
-            # confirmlabel = tkinter.Label()
+            if new_health <= 0 or new_health > 100:
+                tkinter.messagebox.showerror('Out of range', 'You inputted a out of range number')
+            else:
+                player.health = new_health
+                tkinter.messagebox.showinfo('Success!', 'Health Changed Successfully!')
         except ValueError:
-            pass # needs label
+            tkinter.messagebox.showerror('Non number value!', 'ERROR! You have entered a non number value.')
     updatehealthb = tkinter.Button(settingwindow, text="Set Health", command=update_health)
     updatehealthb.place(x=300, y=150)
-   
+    
     #### geeks4geeks ####
     
     def show():
         selected_weapon = cb.get()
-        player.weapon = selected_weapon  # Update the weapon attribute
-        lbl.config(text=f"Weapon set to: {selected_weapon}")  # Update the label
+        player.weapon = selected_weapon 
+        lbl.config(text=f"Weapon set to: {selected_weapon}")  
         print(player.weapon)
+    
+    WeaponSelectPlayer = ["None", "Sword", "Gun"]
 
-    a = ["None", "Sword", "Gun"]
 
-
-    cb = ttk.Combobox(settingwindow, values=a)
-    cb.set("None")
+    cb = ttk.Combobox(settingwindow, values=WeaponSelectPlayer)
+    cb.set(f"{player.weapon}")
     cb.place(x=151, y=250)
     confirmweap = tkinter.Button(settingwindow, text="Set Weapon", command=show)
     confirmweap.place(x=300, y=250)
-# Label to show selected value  
     lbl = tkinter.Label(settingwindow, text="")
-    lbl.place(x=150, y=285)
-    
+    lbl.place(x=390, y=250)
+    weaponlabl = tkinter.Label(settingwindow, text= "Player Weapon:")
+    weaponlabl.place(x=50, y = 250)
     
     #### geeks4geeks ####
 
@@ -102,13 +115,51 @@ def settingspressed():
     
     def updatedamage():
         try:
-            new_damage = int(health_entry.get())
+            new_damage = int(damage_entry.get())
             player.damage = new_damage
             # confirmlabel = tkinter.Label()
         except ValueError:
             pass # needs label
     updatedamageb = tkinter.Button(settingwindow, text="Set Damage", command= updatedamage)
     updatedamageb.place(x=300,y= 200)
+
+        #### ENEMY SETTINGS ####
+        
+    enemylabel = tkinter.Label(settingwindow, text="Enemy", font=('Arial', 25))
+    enemylabel.place(x=160, y= 290)
+
+    #### health setting handler ####
+
+    healthEnemy_label = tkinter.Label(settingwindow, text="Enemy Health:")
+    healthEnemy_label.place(x=50, y=340)
+    healthEnemy_entry = tkinter.Entry(settingwindow)
+    healthEnemy_entry.place(x=160, y=340)
+    def updateEnem_health():
+        try:
+            new_healthEnemy = int(healthEnemy_entry.get())
+            enemy.health = new_healthEnemy
+            # confirmlabel = tkinter.Label()
+        except ValueError:
+            pass # needs label
+    updatehealthb = tkinter.Button(settingwindow, text="Set Health", command=updateEnem_health)
+    updatehealthb.place(x=300, y=339)
+
+    #### damage setting handler ####
+
+    damageEnemylabel = tkinter.Label(settingwindow, text = "Enemy Damage:")
+    damageEnemylabel.place(x=50, y= 390)
+    damageEnemy_entry = tkinter.Entry(settingwindow)
+    damageEnemy_entry.place(x=160, y= 390)
+    
+    def updatedEnemamage():
+        try:
+            newEnemy_damage = int(damageEnemy_entry.get())
+            enemy.damage = newEnemy_damage
+            # confirmlabel = tkinter.Label()
+        except ValueError:
+            pass # needs label
+    updatedamageb = tkinter.Button(settingwindow, text="Set Damage", command= updatedEnemamage)
+    updatedamageb.place(x=300,y= 390)
         
 #### UI buttons handling ####
 
